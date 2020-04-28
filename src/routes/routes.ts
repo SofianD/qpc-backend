4200bd36b50f492f15ac8b2d65b4874741328b97
Pierre Demailly <pierredemailly.pro@gmail.com>
2020-04-28 12:08:17 +0200

    fix: add auth route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import user from './user';
+import auth from './auth';
 
 const routes = Router();
 routes.use('/users', user);
+routes.use('/auth', auth);
 
 export default routes;
